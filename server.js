0151d73a6e63e5b2f54c5adf44a447bd9a6e42b8
glukocerin <ati.csordas@gmail.com>
2016-02-15 10:29:42 +0100

    third commit

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('HI HEROKU!!');
+var config = require('./config.js');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = config.PORT_FOR_SERVER;
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+
+
+app.listen(port, function() {
+  console.log('Listening on port: ' + port);
+});
