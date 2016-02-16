2144fd17c2e339f5e8a92f6f61615e553c62b127
glukocerin <ati.csordas@gmail.com>
2016-02-16 11:57:10 +0100

    add test files

--- spec/databaserequests.spec.js
+++ spec/databaserequests.spec.js
@@ -1,2 +1,15 @@
 'use strict';
 
+var DataBaseRequests = require('../server/dataBaseRequests');
+
+describe('meal', function() {
+  it('should give back one item', function() {
+        
+        function query(query) {
+         expect(query).toEqual('SELECT ok FROM heartbeat');
+        }
+        
+        var dataBaseRequests = new DataBaseRequests(query);
+        dataBaseRequests.heartBeat();
+    });
+});
