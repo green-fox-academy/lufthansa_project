70ddfdbec334e36ed73142206e63e8cfb232ae2f
glukocerin <ati.csordas@gmail.com>
2016-02-16 11:07:16 +0100

    refactor server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 'use strict';
 
-var config = require('./config.js');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+var config = require('./server/config.js');
+var initExpress = require ('./server/initExpressServer.js');
+var dataBaseConnection = require('./server/dataBaseConnection.js');
+
 var port = config.PORT_FOR_SERVER;
-
-app.use(express.static('public'));
-app.use(bodyParser.json());
-
-
+var app = initExpress(dataBaseConnection);
 
 app.listen(port, function() {
   console.log('Listening on port: ' + port);
