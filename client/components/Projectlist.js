a1f2ee8a171841a022f12deea9bd586e76398ea8
Lilla Virag <viraglilla@gmail.com>
2016-02-18 10:41:11 +0100

    try to fix npm issue

--- client/components/Projectlist.js
+++ client/components/Projectlist.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 var ProjectList = React.createClass({
   render: function(){
-    var projects = [1, 2, 3]
+    var projects = [1, 2, 3, 4]
     // var projects = this.props.projects.map(function(item) {
     //   return <li key={item.id}> {item.name} {item.build_status} {item.build_time} </li>
     // });
