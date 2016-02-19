71c98a82607aef33fa435e121e9b14b9d78b685c
Dorottya Hidas <dorottya.hidas@gmail.com>
2016-02-19 13:55:12 +0100

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,6 @@ var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssnext = require('cssnext');
-var precss = require('precss');
 
 gulp.task('jshint', function () {
   return gulp.src('./*.js')
@@ -37,6 +33,6 @@ gulp.task('watch', function () {
   gulp.watch(['./*.js', './*.jsx'], ['jshint', 'jasmine-test', 'jscs-reporter']);
 });
 
-gulp.task('checkAll', ['jshint', 'jasmine-test', 'jscs-reporter', 'css']);
+gulp.task('checkAll', ['jshint', 'jasmine-test', 'jscs-reporter']);
 
 gulp.task('default', ['watch']);
