eab79932e45f810a6b201a57603eb9b0c79c3b6d
Dorottya Hidas <dorottya.hidas@gmail.com>
2016-02-22 17:46:53 +0100

    create table for projects list

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
-var ProjectList = require('./Projectlist')
+var ProjectList = require('./Projectlist');
+require('../style/modules/container.css');
 
 var Projects = React.createClass({
   render: function(){
@@ -7,6 +8,22 @@ var Projects = React.createClass({
       <div>
         <h3>Projects</h3>
         <ProjectList projects={this.props.projects} />
+        <table className="fullTable">
+		<tr className="firstRow">
+			<td className="projectName">reallylooooooooooooooongprojectnamefortest</td>
+			<td className="circle">
+				<span className="greenBuild"></span>
+			</td>
+			<td className="buildDate">2/22/2016 14:01</td>
+		</tr>
+		<tr className="secondRow">
+			<td className="projectName">shortpprojectname</td>
+			<td className="circle">
+				<span className="redBuild"></span>
+			</td>
+			<td className="buildDate">2/22/2016 14:13</td>
+		</tr>
+		</table>
       </div>
     )
   }
