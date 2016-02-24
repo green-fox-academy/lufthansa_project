3cedd69b8e654a9807ef98eaeeb9c8c70cedb4d3
Lilla Virag <viraglilla@gmail.com>
2016-02-24 13:48:23 +0100

    putting back mockup list

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -8,6 +8,24 @@ var Projects = React.createClass({
       <div>
         <h3>Projects</h3>
         <ProjectList projects={this.props.projects} />
+        <table>
+          <tbody>
+            <tr className="firstRow">
+              <td className="projectName">reallylooooooooooooooongprojectnamefortest</td>
+              <td className="circle">
+               <span className="greenBuild"></span>
+              </td>
+              <td className="buildDate">2/22/2016 14:01</td>
+            </tr>
+            <tr className="secondRow">
+              <td className="projectName">shortpprojectname</td>
+               <td className="circle">
+                 <span className="redBuild"></span>
+               </td>
+               <td className="buildDate">2/22/2016 14:13</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
