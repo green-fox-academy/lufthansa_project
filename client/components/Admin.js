3395b7166879f007e98987958cf38897cdba38a5
Matyas Csiki <matyas.csiki@gmail.com>
2016-02-24 13:53:10 +0100

    basic admin form and table added

--- client/components/Admin.js
+++ client/components/Admin.js
@@ -1,11 +1,94 @@
+'use strict';
+
 import React from 'react';
+var fetchRequest = require('../http-request');
+var url = window.location.origin + '/api/projects';
 
 var Admin = React.createClass({
-  render: function(){
+  renderName: 'Admin',
+  getInitialState: function () {
+    return {
+      projectList: [
+       { name: 'project_1', path: '/path' },
+       { name: 'project_2', path: '/path' },
+      ],
+      name:'',
+      path:'',
+    };
+  },
+
+  componentDidMount: function () {
+    this.getProjectList();
+  },
+
+  getProjectList: function () {
+    // fetchRequest('GET', url, null, this.updateList);
+  },
+
+  updateList: function (response) {
+    // this.setState( projectList: response });
+    console.log(this.state.name + '         ' + this.state.path);
+  },
+
+  handleNameChange: function (event) {
+    this.setState({ name: event.target.value });
+  },
+
+  handlePathChange: function (event) {
+    this.setState({ path: event.target.value });
+  },
+
+  handleSubmit: function (event) {
+    event.preventDefault();
+    console.log(this.state.name + ' ' + this.state.path);
+
+    // var dataToObject = {
+    //   name: this.state.name,
+    //   calorie: this.state.calorie,
+    //   date: this.state.date
+    // };
+    // var data = JSON.stringify(dataToObject);
+    // createRequest('POST', url, data, this.getItems);
+    // }
+  },
+
+  render: function () {
     return (
-      <h3>Admin form</h3>
+        <div>
+          <h3>Admin form</h3>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" onChange={this.handleNameChange} placeholder="Project name" />
+            <input type="text" onChange={this.handlePathChange} placeholder="Path" />
+            <button>ADD</button>
+          </form>
+          <ProjectTable projectList={this.state.projectList}/>
+        </div>
+    );
+  }
+});
+
+var ProjectTable = React.createClass({
+  renderName: 'projectTable',
+  render: function (response) {
+  var createProjectLine = function (project) {
+    return (
+      <tr key={project.name}>
+        <td>{project.name}</td>
+        <td>{project.path}</td>
+        <td><button>EDIT</button></td>
+        <td><button>DELETE</button></td>
+      </tr>
     )
   }
-})
+
+  return (
+    <table>
+      <tbody>
+        {this.props.projectList.map(createProjectLine)}
+      </tbody>
+    </table>
+    );
+  }
+});
 
 module.exports = Admin;
