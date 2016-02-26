45bffc6508be339760dd2b14d313af6e45d7ada0
glukocerin <ati.csordas@gmail.com>
2016-02-26 13:48:23 +0100

    create new file for request

---
+++ server/templateForResponse.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function responseTemplate(result) {
+  var resultArray = [];
+  result.rows.forEach(function (build) {
+  var buildToObject = {
+    projects: [
+      {
+        name: build.project_name,
+        id: build.project_id,
+        projectUrl: build.project_url,
+        lastBuild: {
+          status: build.build_status,
+          time: build.build_date,
+          coverage: {
+            totalLines: build.build_totallines,
+            actualLines: build.build_actuallines,
+          },
+        },
+      },
+    ],
+    status: 'ok',
+  };
+  resultArray.push(buildToObject);
+  });
+  return resultArray; 
+}
+
+module.exports = responseTemplate;
