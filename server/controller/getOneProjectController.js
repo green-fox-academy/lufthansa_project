c090655ca5e0ec3201e2b6efcdc1514c84007241
glukocerin <ati.csordas@gmail.com>
2016-02-26 14:06:01 +0100

    refactor

--- server/controller/getOneProjectController.js
+++ server/controller/getOneProjectController.js
@@ -4,11 +4,11 @@ var resposeTemplate = require('../templateForRequest.js');
 
 function GetOneProjectController(query) {
 
-	this.getOneProject = function (request, response) {
-		query.getOneProject(request.params.id, function (err, result) {
-			if (err) {
-				response.status(500).json({ 'problem with database connection': err });
-			} else {
+  this.getOneProject = function (request, response) {
+    query.getOneProject(request.params.id, function (err, result) {
+      if (err) {
+        response.status(500).json({ 'problem with database connection': err });
+      } else {
         response.status(200).json(resposeTemplate(result));
       }
     });
