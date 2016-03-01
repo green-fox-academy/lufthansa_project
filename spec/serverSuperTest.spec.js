695364e612e5cda5b7ebb133f05b0a94a5c03502
glukocerin <ati.csordas@gmail.com>
2016-03-01 11:46:24 +0100

    add new database queries and supertests

--- spec/serverSuperTest.spec.js
+++ spec/serverSuperTest.spec.js
@@ -20,3 +20,37 @@ describe('when GET req sent', function () {
       });
   });
 });
+
+describe('when GET req sent', function () {
+  var query = function(query, callback) {
+    callback(null, { rows: [{}] });
+  };
+  it('response format should be JSON', function(done) {
+    var app = initExpressServer(query);
+    request(app)
+        .get('/api/projects')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (err, res) {
+        if (err) throw err;
+        done();
+      });
+  });
+});
+
+describe('when GET req sent', function () {
+  var query = function(query, callback) {
+    callback(null, { rows: [{}] });
+  };
+  it('response format should be JSON', function(done) {
+    var app = initExpressServer(query);
+    request(app)
+        .get('/loglevel')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (err, res) {
+        if (err) throw err;
+        done();
+      });
+  });
+});
