1382ff1103ee0b49c2d89e2ee7a71824301c53fe
Lilla Virag <viraglilla@gmail.com>
2016-03-03 11:35:57 +0100

    correct jasmine database query test

--- spec/databaserequests.spec.js
+++ spec/databaserequests.spec.js
@@ -4,11 +4,11 @@ var DataBaseRequests = require('../server/databaseRequests');
 
 describe('heartbeat', function() {
   it('should give back one item', function() {
-        
+
         function query(query) {
          expect(query).toEqual('SELECT ok FROM heartbeat');
         }
-        
+
         var dataBaseRequests = new DataBaseRequests(query);
         dataBaseRequests.heartBeat();
     });
@@ -16,11 +16,11 @@ describe('heartbeat', function() {
 
 describe('get all projects query', function() {
   it('should give back one item', function() {
-        
+
         function query(query) {
-         expect(query).toEqual('SELECT * FROM projects INNER JOIN builds ON (projects.project_id = builds.project_id) WHERE project_is_visible = TRUE');
+         expect(query).toEqual('SELECT * FROM projects INNER JOIN (SELECT distinct on (project_id) MAX(build_date), project_id, build_id, build_actuallines, build_totallines, build_status FROM builds GROUP BY project_id, build_id, build_actuallines, build_totallines, build_status) AS b ON(b.project_id=projects.project_id) WHERE project_is_visible = TRUE');
         }
-        
+
         var dataBaseRequests = new DataBaseRequests(query);
         dataBaseRequests.getAllProjects();
     });
@@ -28,11 +28,11 @@ describe('get all projects query', function() {
 
 describe('get one projects query', function() {
   it('should give back one item', function() {
-        
+
         function query(query) {
          expect(query).toEqual('SELECT * FROM projects INNER JOIN builds ON (projects.project_id = builds.project_id) WHERE builds.project_id = $1');
         }
-        
+
         var dataBaseRequests = new DataBaseRequests(query);
         dataBaseRequests.getOneProject();
     });
@@ -40,11 +40,11 @@ describe('get one projects query', function() {
 
 describe('get one projects query', function() {
   it('should give back one item', function() {
-        
+
         function query(query) {
          expect(query).toEqual('UPDATE projects SET project_is_visible = false WHERE project_id= $1');
         }
-        
+
         var dataBaseRequests = new DataBaseRequests(query);
         dataBaseRequests.changeVisibility();
     });
