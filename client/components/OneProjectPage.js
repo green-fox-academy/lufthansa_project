40c4ee6bb6ce3cd673b416b38e649064f6331638
csmaty <matyas.csiki@gmail.com>
2016-03-09 09:27:17 +0100

    coverage progress changes color based on percentage

--- client/components/OneProjectPage.js
+++ client/components/OneProjectPage.js
@@ -18,6 +18,7 @@ var OneProject = React.createClass({
 			name: '',
 			totalLines: 0,
 			coveredLines: 0,
+			progressBarColor: null
 		};
 	},
 
@@ -37,6 +38,16 @@ var OneProject = React.createClass({
 			coveredLines: Number(project[0].lastBuild.coverage.actualLines),
 			totalLines: Number(project[0].lastBuild.coverage.totalLines)
 		});
+		this.setProggressBarColor();
+	},
+
+	setProggressBarColor: function () {
+		console.log(this.state.coveredLines / this.state.totalLines);
+		if ((this.state.coveredLines / this.state.totalLines) < 0.8) {
+			this.setState({
+				progressBarColor: "#F48D7D"
+			})
+		}
 	},
 
 	render: function() {
@@ -46,7 +57,7 @@ var OneProject = React.createClass({
 					<div className="projectNameTitle">{this.state.name}</div>
 				<h4 className="coverage">Coverage: </h4>
 					<div className="projectCoverage">{this.state.coveredLines} / {this.state.totalLines}</div>				
-						<Progress completed={this.state.coveredLines / this.state.totalLines * 100} />
+					<Progress completed={this.state.coveredLines / this.state.totalLines * 100} color={this.state.progressBarColor}/>
 			</div>);
 	},
 });
